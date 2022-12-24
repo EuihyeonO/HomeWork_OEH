0ba6cb62f31173fac77a4fe2a71bb62b34ba94dc
EuihyeonO <yuu5666@naver.com>
2022-12-24 22:11:48 +0900

    주석추가

--- HomeWork_OEH/HomeWork/SnakeGame/Body.cpp
+++ HomeWork_OEH/HomeWork/SnakeGame/Body.cpp
@@ -10,7 +10,7 @@ void Body::CreateBody(int4 _pos)
 {
 	srand(time(nullptr));
 
-	CurBody[NumOfCurBody] = new Body;
+	CurBody[NumOfCurBody] = new Body; //릭을 없애기 위해 배열형으로 선언하여, 인덱스당 하나씩 동적할당
 
 	int4 BodyPos = { rand() % ConsoleGameScreen::GetMainScreen()->GetScreenSize().X, rand() % ConsoleGameScreen::GetMainScreen()->GetScreenSize().Y };
 
@@ -27,25 +27,28 @@ void Body::CreateBody(int4 _pos)
 			if (i == NumOfCurBody)
 			{
 				break;
-			}
+			} //모든 바디의 위치를 전부 다 탐색했을 시 반복문 종료
 
 			if (BodyPos == CurBody[i]->GetPos() || BodyPos == _pos)
 			{
 				isSame = true;
 				break;
-			}
+			} //같은 위치의 바디가 있다면 IsSame을 true로 값을 변경한 뒤 반복문 종료
+
 			++i;
+
 		}
 
 		if (isSame == false)
 		{
 			break;
-		}
+		} //모든 위치를 탐색 후, 같은 위치에 있는 바디가 없다면, 반복문 종료
 
 		BodyPos = { rand() % ConsoleGameScreen::GetMainScreen()->GetScreenSize().X, rand() % ConsoleGameScreen::GetMainScreen()->GetScreenSize().Y };
+		//바디와 동일한 위치에 새로운 바디가 생성될시 BodyPos 의 위치를 다시 랜덤으로 찍어낸 후 반복문 계속 실행
 	}
 
-	CurBody[NumOfCurBody]->SetPos(BodyPos);
+	CurBody[NumOfCurBody]->SetPos(BodyPos); //겹치는 곳이 없는 것이 확인된 BodyPos에 새로운 바디 생성
 
 	++NumOfCurBody;
 }
